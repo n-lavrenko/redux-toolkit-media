4c4df96503dd3c8312ae44d9f1c9fda23449bee4
Nikita Lavrenko <lavrenonik@gmail.com>
2023-10-17 17:35:50 +0400

    Fetch users + show them on UserList

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,5 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUsers } from '../store';
+import { useEffect } from 'react';
+
 function UsersList() {
-	return 'Users List'
+  const dispatch = useDispatch();
+  const {isLoading, data, error} = useSelector((state) => state.users);
+	
+  useEffect(() => {
+    dispatch(fetchUsers());
+		console.log('i fire once');
+  }, [dispatch]);
+
+  const renderedUsers = data.map((user) => (
+    <div key={user.id} className='p-3 shadow-md'>
+      {user.id}: {user.name}
+    </div>
+  ));
+
+	if (isLoading) {
+		return <div>Loading...</div>
+	}
+
+	if (error) {
+		return <div>Error fetching data</div>
+	}
+
+  return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
