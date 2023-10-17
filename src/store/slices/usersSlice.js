f0572a5fcfe5a6bdee21432d4b4c993eb5a14036
Nikita Lavrenko <lavrenonik@gmail.com>
2023-10-17 13:55:22 +0400

    Initial commit

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -1,16 +1,26 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchUsers } from '../thunks/fetchUsers';
 
 const usersSlice = createSlice({
 	name: 'users',
 	initialState: {
-		data: []
+		data: [],
+		isLoading: false,
+		error: null
 	},
-	reducers: {
-		getUsers(state, action) {
-			state = [];
-		}
+	extraReducers(builder) {
+		builder.addCase(fetchUsers.pending, (state, action) => {
+			state.isLoading = true;
+		});
+		builder.addCase(fetchUsers.fulfilled, (state, action) => {
+			state.isLoading = false;
+			state.data = action.payload;
+		});
+		builder.addCase(fetchUsers.rejected, (state, action) => {
+			state.isLoading = false;
+			state.error = action.error;
+		});
 	}
 })
 
 export const usersReducer = usersSlice.reducer;
-export const { getUsers } = usersSlice.actions
