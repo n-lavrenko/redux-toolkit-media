2dece1deed3960c97648c7cf90baebbd6213db90
Nikita Lavrenko <lavrenonik@gmail.com>
2023-10-17 18:04:10 +0400

    Skeleton is done. Small css changes in list of users.

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,29 +1,30 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 import { useEffect } from 'react';
+import Skeleton from './Skeleton';
 
 function UsersList() {
   const dispatch = useDispatch();
-  const {isLoading, data, error} = useSelector((state) => state.users);
-	
+  const { isLoading, data, error } = useSelector((state) => state.users);
+
   useEffect(() => {
     dispatch(fetchUsers());
-		console.log('i fire once');
+    console.log('i fire once');
   }, [dispatch]);
 
   const renderedUsers = data.map((user) => (
-    <div key={user.id} className='p-3 shadow-md'>
-      {user.id}: {user.name}
+    <div key={user.id} className='mb-2 border p-2 shadow-md rounded'>
+      <div className='flex p-2 justify-between items-center cursor-pointer'>{user.name}</div>
     </div>
   ));
 
-	if (isLoading) {
-		return <div>Loading...</div>
-	}
+  if (isLoading) {
+    return <Skeleton times={6} className='h-10 w-full' />;
+  }
 
-	if (error) {
-		return <div>Error fetching data</div>
-	}
+  if (error) {
+    return <div>Error fetching data</div>;
+  }
 
   return <div>{renderedUsers}</div>;
 }
