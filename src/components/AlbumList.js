ef39334c441efa7f809844f534ac927403f727af
Nikita Lavrenko <lavrenonik@gmail.com>
2023-10-18 15:02:42 +0400

    Fetching user's albums through RTK Query, rendered list of albums.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,3 +1,28 @@
-export default function AlbumList({user}) {
-	return <div>Albums for {user.name}</div>
+import { useFetchAlbumsQuery } from '../store';
+import Skeleton from './Skeleton';
+import ExpandeblePanel from './ExpandablePanel';
+
+export default function AlbumList({ user }) {
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton times={3} />;
+  } else if (error) {
+    content = <div>Error while loading user albums</div>;
+  } else {
+		content = data.map((album) => {
+			const header = <div>{album.title}</div>;
+      return <ExpandeblePanel header={header}>
+        List of photos in the album
+      </ExpandeblePanel>;
+    });
+  }
+
+  return (
+    <>
+      <div className='pb-3'>Albums for {user.name}</div>
+      <div>{content}</div>
+    </>
+  );
 }
